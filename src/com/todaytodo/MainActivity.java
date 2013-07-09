ed345e7b3d37373a8f303d77dfcf4e1eaf3d94f2
bluestonest <shentongss@hotmail.com>
2013-07-09 12:56:28 +0800

    commit a new tip

--- src/com/todaytodo/MainActivity.java
+++ src/com/todaytodo/MainActivity.java
@@ -79,6 +79,12 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
+/**
+ * 
+ * @author Sunny
+ *
+ */
+
 public class MainActivity extends ActivityBase {
 	//public data elements
 	private ThingList thingList = new ThingList();
